a7e3fe9981d4bb81fa4f348e5559182ce17f0ae6
JornWindesheim <s1217772@student.windesheim.nl>
2025-09-15 16:36:41 +0200

    - Added code commentary for AddProduct()

--- Grocery.App/ViewModels/GroceryListItemsViewModel.cs
+++ Grocery.App/ViewModels/GroceryListItemsViewModel.cs
@@ -68,13 +68,6 @@ namespace Grocery.App.ViewModels
         [RelayCommand]
         public void AddProduct(Product product)
         {
-            //Controleer of het product bestaat en dat de Id > 0
-            //Maak een GroceryListItem met Id 0 en vul de juiste productid en grocerylistid
-            //Voeg het GroceryListItem toe aan de dataset middels de _groceryListItemsService
-            //Werk de voorraad (Stock) van het product bij en zorg dat deze wordt vastgelegd (middels _productService)
-            //Werk de lijst AvailableProducts bij, want dit product is niet meer beschikbaar
-            //call OnGroceryListChanged(GroceryList);
-
             // Controleer of het product bestaat en dat de Id > 0
             if (product == null || product.Id <= 0) return;
 
