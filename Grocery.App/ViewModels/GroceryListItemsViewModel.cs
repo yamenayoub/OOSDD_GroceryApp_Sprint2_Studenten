91ad4fed81685fd557b77aea9b93c3d4c748863e
JornWindesheim <s1217772@student.windesheim.nl>
2025-09-15 16:32:58 +0200

    - Added some code for AddProduct()

--- Grocery.App/ViewModels/GroceryListItemsViewModel.cs
+++ Grocery.App/ViewModels/GroceryListItemsViewModel.cs
@@ -74,6 +74,25 @@ namespace Grocery.App.ViewModels
             //Werk de voorraad (Stock) van het product bij en zorg dat deze wordt vastgelegd (middels _productService)
             //Werk de lijst AvailableProducts bij, want dit product is niet meer beschikbaar
             //call OnGroceryListChanged(GroceryList);
+
+            // Controleer of het product bestaat en dat de Id > 0
+            if (product == null || product.Id <= 0) return;
+
+            // Maak een GroceryListItem met Id 0 en vul de juiste productid en grocerylistid
+            var groceryListItem = new GroceryListItem(0, GroceryList.Id, product.Id, 1);
+
+            // Voeg het GroceryListItem toe aan de dataset middels de _groceryListItemsService
+            _groceryListItemsService.Add(groceryListItem);
+
+            // Werk de voorraad (Stock) van het product bij en zorg dat deze wordt vastgelegd (middels _productService)
+            product.Stock--;
+            _productService.Update(product);
+
+            // Werk de lijst AvailableProducts bij, want dit product is niet meer beschikbaar
+            AvailableProducts.Remove(product);
+
+            // call OnGroceryListChanged(GroceryList)
+            OnGroceryListChanged(GroceryList);
         }
     }
 }
